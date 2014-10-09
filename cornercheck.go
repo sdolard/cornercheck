91efabb84bdeb2a67483ed195608548a1f9da9f9
sdolard <sdolard@gmail.com>
2014-10-09 17:02:46 +0200

    addCookies

--- cornercheck.go
+++ cornercheck.go
@@ -5,35 +5,60 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"net/http/cookiejar"
+	"net/url"
 )
 
-// GET /ventes_immobilieres/offres/rhone_alpes/rhone/?f=a&th=1&ret=1 HTTP/1.1
+// GET /voitures/offres/rhone_alpes/rhone/?f=p&th=1&ps=8&pe=9&ms=50000&me=125000 HTTP/1.1
 // Host: www.leboncoin.fr
 // Connection: keep-alive
-// Pragma: no-cache
-// Cache-Control: no-cache
 // Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8
-// User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.94 Safari/537.36
-// Referer: http://www.leboncoin./ventes_immobilieres/offres/rhone_alpes/rhone/?f=a&th=1&ret=1
+// User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.101 Safari/537.36
+// Referer: http://www.leboncoin.fr/voitures/offres/rhone_alpes/ain/?f=p&th=1&ps=8&pe=9&ms=50000&me=125000
 // Accept-Encoding: gzip,deflate,sdch
 // Accept-Language: fr-FR,fr;q=0.8,en-US;q=0.6,en;q=0.4
-// Cookie:
-// layout=0;
-// 	xtvrn=$266818$;
-// 	s=red1x3aa997d8f544b3f4132f0ea0d22046956f12a198;
-// 	OAX=Wh1JgFQJ8IcAA7hF;
-// 	weboForOas={"weboQueryDate":"2014-09-06-12-26","weboCalls":2,"clusters":"","social_demo":"","oasCalls":1};
-// 	lazyLoadCounterAppear=13;
-// 	location_search_22_169_anse_69480=Anse%2069480:8;
-// 	sq=ca=22_s&w=169&c=9&f=a&th=1&ret=1;
-// 	cookieFrame=2;
-// 	RMFD=011XQDCh6B06BZ|6207QX|6307px|61088K|62089t;
-// 	RMFS=011XQHtcU2080N;
-// 	is_new_search=1
+// Cookie: xtvrn=$266818$;
+//  OAX=Wh1ajFQmtwAADbeR;
+//  location_search_22_1_toutes_les_communes_01600=Toutes%20les%20communes%2001600:3;
+//  location_search_22_1_toutes_les_communes_69480=Toutes%20les%20communes%2069480:1;
+//  sli=1;
+//  lazyLoadCounterAppear=39;
+//  weboForOas={"weboQueryDate":"2014-10-09-09-38","weboCalls":2,"clusters":"","audiences":"","social_demo":"","oasCalls":1};
+//  RMFD=011Xc8J2O205fc!O107aY!O307aZ!O108WN!S208ZY!B508fi;
+//  layout=0;
+//  s=red1x490e57f2ad31c85f5fc275aa0354d81abdde2062;
+//  sq=ca=22_s&w=101&c=2&f=p&th=1&ps=8&pe=9&ms=50000&me=125000;
+//  cookieFrame=2;
+//  is_new_search=1
+
+// GET /voitures/offres/rhone_alpes/ain/?f=p&th=1&ps=8&pe=9&ms=50000&me=125000 HTTP/1.1
+// Host: www.leboncoin.fr
+// Connection: keep-alive
+// Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8
+// User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.101 Safari/537.36
+// Referer: http://www.leboncoin.fr/voitures/offres/rhone_alpes/rhone/?f=p&th=1&ps=8&pe=9&ms=50000&me=125000
+// Accept-Encoding: gzip,deflate,sdch
+// Accept-Language: fr-FR,fr;q=0.8,en-US;q=0.6,en;q=0.4
+// Cookie: xtvrn=$266818$;
+//  OAX=Wh1ajFQmtwAADbeR;
+//  location_search_22_1_toutes_les_communes_01600=Toutes%20les%20communes%2001600:3;
+//  location_search_22_1_toutes_les_communes_69480=Toutes%20les%20communes%2069480:1;
+//  sli=1;
+//  lazyLoadCounterAppear=39;
+//  weboForOas={"weboQueryDate":"2014-10-09-09-38","weboCalls":2,"clusters":"","audiences":"","social_demo":"","oasCalls":1};
+//  RMFD=011Xc8J2O205fc!O107aY!O307aZ!O108WN!S208ZY!B508fi;
+//  layout=0;
+//  sq=ca=22_s&w=169&c=2&f=p&th=1&ps=8&pe=9&ms=50000&me=125000;
+//  cookieFrame=2;
+//  s=red1x490e57f2ad31c85f5fc275aa0354d81abdde2062;
+//  is_new_search=1
 
 func main() {
-	c := &http.Client{}
-	s, err := runReq(c, "http://www.leboncoin.fr/ventes_immobilieres/offres/rhone_alpes/rhone/?f=a&th=1&ret=1")
+	cookieJar, _ := cookiejar.New(nil)
+	c := &http.Client{
+		Jar: cookieJar,
+	}
+	s, err := runReq(c, "http://www.leboncoin.fr/voitures/offres/rhone_alpes/rhone/?f=p&th=1&ps=8&pe=9&ms=50000&me=125000")
 	if err != nil {
 		log.Printf("error reading string: %v", err)
 		return
@@ -41,14 +66,27 @@ func main() {
 	fmt.Printf("%v\n", s)
 }
 
-func addCookies(c *http.Client) {
-	url := &c.Jar.SetCookies(u, cookies)
+func addCookies(c *http.Client, u string) error {
+	parsedUrl, err := url.Parse(u)
+	if err != nil {
+		log.Printf("error parsing string: %v", u)
+		return err
+	}
+	cookies := []*http.Cookie{
+		{Name: "sq", Value: "ca=22_s&w=101&c=2&f=p&th=1&ps=8&pe=9&ms=50000&me=125000", Path: "/", Domain: ".leboncoin.fr"},
+	}
+
+	c.Jar.SetCookies(parsedUrl, cookies)
+	return nil
 }
 
-func runReq(c *http.Client, url string) (string, error) {
-	addCookies(c)
+func runReq(c *http.Client, u string) (string, error) {
+	//err := addCookies(c, u)
+	// if err != nil {
+	// 	return "", err
+	// }
 
-	resp, err := c.Get(url)
+	resp, err := c.Get(u)
 	if err != nil {
 		return "", err
 	}
